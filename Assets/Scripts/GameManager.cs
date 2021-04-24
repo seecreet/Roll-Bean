08c53036b419430d3cd3f5480460de881acc127e
James <jesseserre@gmail.com>
2021-04-24 19:48:18 -0400

    UI Layout, Linked D10000 Table

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public TextAsset table;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       /* int counter = 0;
+        rolls = new String[10001];
+        try
+        {
+            InputStream is = getAssets().open("dtenthousandtable.txt");
+            InputStreamReader r = new InputStreamReader(is);
+            try(BufferedReader br = new BufferedReader(r)) {
+                String line = br.readLine();
+
+                while (line != null)
+                {
+                    rolls[counter] = line;
+                    counter++;
+                    line = br.readLine();
+                }
+            } catch (FileNotFoundException e)
+            {
+                e.printStackTrace();
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+        catch (java.io.IOException e) { e.printStackTrace(); }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {*/
+        
+    }
+}
