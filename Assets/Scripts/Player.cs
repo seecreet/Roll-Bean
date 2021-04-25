698b91729108e578c17300a3638ef995d6acced3
James <jesseserre@gmail.com>
2021-04-25 14:57:58 -0400

    Logic and Efficiency Updates, Networking Init

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAPI;
+
+public class Player : NetworkBehaviour
+{
+    public int health;
+    public int speed;
+    public int height;
+
+    public string race;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetAll(int hp, int sp, int hei, string rac)
+    {
+        health = hp;
+        speed = sp;
+        height = hei;
+        race = rac;
+    }
+
+    public void SetHealth(int hp)
+    {
+        health = hp;
+    }
+
+    public void SetSpeed(int sp)
+    {
+        speed = sp;
+    }
+
+    public void SetHeight(int hei)
+    {
+        height = hei;
+    }
+
+    public void SetRace(string rac)
+    {
+        race = rac;
+    }
+
+}
